a14fab354d541bb3f9226baaa4957228ed03c6e2
Yahyaajbali <49082256+Yahyaajbali@users.noreply.github.com>
2020-03-23 20:56:31 +0100

    modified
    added an indicator that shows if the number is higher or lower than the one generated

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,17 +1,23 @@
 from random import *
 
 user_input = 30
-while user_input < 0 or user_input > 20:
-    s = input("guess the number from 0 to 20 ")
-
-    user_input = int(s)
-
 
 v = randint(0, 20)
 random_number = int(v)
 
-if user_input == random_number:
-    print("CONGRATS, YOU GUESSED RIGHT")
-else:
-    print("GOOD LUCK NEXT TIME, THE COMPUTER CHOSE ")
-    print(random_number)
+for count in range(5):
+    s = input("guess the number from 0 to 20 ")
+
+    user_input = int(s)
+
+    if user_input == random_number:
+        print("CONGRATS, YOU GUESSED RIGHT")
+        break
+    elif user_input > random_number:
+        print("too high")
+    else:
+        print("too low")
+    if count == 4:
+        print("lost, number was", random_number)
+
+
